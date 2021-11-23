27b278e6255646bb8aba8a2dbb189e01a5d5969e
Crislip, Don <DCrislip@indians.com>
2021-11-23 12:41:47 -0500

    Removed pitches svc

--- api/pitches.ts
+++
@@ -1,6 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import { pitches } from "../data/Pitches"
-
-export default (request: VercelRequest, response: VercelResponse) => {
-  response.status(200).send(pitches);
-};
